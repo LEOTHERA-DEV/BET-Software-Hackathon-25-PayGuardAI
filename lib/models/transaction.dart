c6e91d5580209402dab3c75a9463e051fbf3e4e7
Kayleigh Ncube <kayleigh.kasitra@gmail.com>
2025-08-16 10:27:15 +0200

    Saving changes attempting to resolve issue for team member added model: Transaction

---
+++ lib/models/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final double amount;
+  final String merchant;
+  bool fraudulent;
+  bool synced;
+
+  Transaction({
+    required this.merchant,
+    required this.amount,
+    this.fraudulent = false,
+    this.synced = false,
+  });
+}
